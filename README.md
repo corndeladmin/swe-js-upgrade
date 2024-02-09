ed9b728357cd32003c4ced513c74a1a7791378c3
Andy Huang <cerberus@ayxh.dev>
2024-02-09 04:23:14 +0000

    Refactored tests a bit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Upgrade, upgrade, upgrade!
+
+Welcome to the hit new game, _Upgrade, upgrade, upgrade!_
+
+In this game, you play a mining robot. Mine minerals, sell them at the surface, and use the money to upgrade your robot. The more you upgrade, the more you can mine, and the more you can mine, the more you can upgrade!
+
+## Getting started
+
+1. Make sure you have `node`, `npm`, and `git` installed on your computer.
+2. Clone this repository so you have it locally.
+3. Run `npm install` to install the game's dependencies.
+
+## Deep dive
+
+### Basic Objects
+
+1. Read the docs on [objects as key value stores]().
+2. Complete Exercise 1a.
+3. Complete Exercise 1b.
+
+### Lists of objects
+
+1. Read the docs on [lists of objects]().
+2. Complete Exercise 2a.
+3. Complete Exercise 2b.
+
+### Object nesting
+
+1. Read the docs on [object nesting]().
+2. Complete Exercise 3.
+
+### Reduce
+
+1. Read the docs on [reduce]().
+2. Complete Exercise 4.
---
+++ exercises/exercise-4.js
@@ -0,0 +1,56 @@
+/**
+ * @typedef Player
+ * @type {object}
+ * @property {number} money - The amount of money the player has
+ * @property {number} depth - The depth the player is at
+ * @property {Array.<string>} inventory - The player's inventory
+ * @property {Array.<string>} upgrades - The upgrades the player has
+ */
+
+/**
+ * @typedef Mineral
+ * @type {object}
+ * @property {string} id - The id used to uniquely identify the mineral
+ * @property {string} name - The name of the mineral
+ * @property {number} value - The value of the mineral
+ * @property {Object} distribution - The distribution of the mineral
+ * @property {number} distribution.minDepth - The minimum depth where the mineral can be found
+ * @property {number} distribution.maxDepth - The maximum depth where the mineral can be found
+ * @property {number} distribution.abundance - The abundance of the mineral
+ * @property {number} hardness - The strength required to mine this mineral
+ *
+ * @example
+ * const exampleMineral = {
+ *   id: 'copper',
+ *   name: 'copper ore',
+ *   value: 24,
+ *   distribution: {
+ *     minDepth: 10,
+ *     maxDepth: 60,
+ *     abundance: 10,
+ *   },
+ *   hardness: 5,
+ * }
+ */
+
+/**
+ * Gets the value of a player's inventory
+ * @example
+ * const player = {
+ *   inventory: ['copper', 'copper', 'iron'],
+ * }
+ * const minerals = {
+ *   copper: {
+ *     value: 5,
+ *   },
+ *   iron: {
+ *     value: 20,
+ *   }
+ * }
+ * // returns 5 + 5 + 20 = 30
+ * valueInventory(player, minerals)
+ * 
+ */
+function valueInventory(player, minerals) {
+  // TODO
+}
