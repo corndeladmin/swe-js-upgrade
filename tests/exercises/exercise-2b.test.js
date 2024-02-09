e3174a6c6b4e6f6b53097028153966564bbc8b5b
Andy Huang <cerberus@ayxh.dev>
2024-02-09 11:55:06 +0000

    Lint!

--- tests/exercises/exercise-2b.test.js
+++ tests/exercises/exercise-2b.test.js
@@ -1,29 +1,29 @@
-import { strict as assert } from 'node:assert';
-import { getAffordableUpgrades } from '../../exercises/exercise-2b.js';
+import { strict as assert } from "node:assert"
+import { getAffordableUpgrades } from "../../exercises/exercise-2b.js"
 
-describe('getAffordableUpgrades', () => {
-    it('should return an array of affordable upgrades', () => {
-        const player = {
-            money: 100,
-        };
-        const upgrades = [
-            {
-                id: 'rocket_boots',
-                value: 50,
-            },
-            {
-                id: 'diamond_pickaxe',
-                value: 100,
-            },
-            {
-                id: 'quantum_drive',
-                value: 200,
-            },
-        ];
-        const expectedUpgrades = ['rocket_boots', 'diamond_pickaxe'];
+describe("getAffordableUpgrades", () => {
+  it("should return an array of affordable upgrades", () => {
+    const player = {
+      money: 100,
+    }
+    const upgrades = [
+      {
+        id: "rocket_boots",
+        value: 50,
+      },
+      {
+        id: "diamond_pickaxe",
+        value: 100,
+      },
+      {
+        id: "quantum_drive",
+        value: 200,
+      },
+    ]
+    const expectedUpgrades = ["rocket_boots", "diamond_pickaxe"]
 
-        const affordableUpgrades = getAffordableUpgrades(player, upgrades);
+    const affordableUpgrades = getAffordableUpgrades(player, upgrades)
 
-        assert.deepStrictEqual(affordableUpgrades, expectedUpgrades);
-    });
-});
+    assert.deepStrictEqual(affordableUpgrades, expectedUpgrades)
+  })
+})
--- tests/exercises/exercise-4.test.js
+++ tests/exercises/exercise-4.test.js
@@ -1,23 +1,23 @@
-import { strict as assert } from 'node:assert';
-import { valueInventory } from '../../exercises/exercise-4.js';
+import { strict as assert } from "node:assert"
+import { valueInventory } from "../../exercises/exercise-4.js"
 
-describe('valueInventory', () => {
-    it('should calculate the value of a player\'s inventory correctly', () => {
-        const player = {
-            inventory: ['copper', 'copper', 'iron'],
-        };
-        const minerals = [
-            {
-                id: 'copper',
-                value: 5,
-            },
-            {
-                id: 'iron',
-                value: 20,
-            },
-        ];
-        const expectedValue = 5 + 5 + 20;
-        const actualValue = valueInventory(player, minerals);
-        assert.strictEqual(actualValue, expectedValue);
-    });
-});
+describe("valueInventory", () => {
+  it("should calculate the value of a player's inventory correctly", () => {
+    const player = {
+      inventory: ["copper", "copper", "iron"],
+    }
+    const minerals = [
+      {
+        id: "copper",
+        value: 5,
+      },
+      {
+        id: "iron",
+        value: 20,
+      },
+    ]
+    const expectedValue = 5 + 5 + 20
+    const actualValue = valueInventory(player, minerals)
+    assert.strictEqual(actualValue, expectedValue)
+  })
+})
